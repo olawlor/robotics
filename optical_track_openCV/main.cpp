daede92de8e423441b35b8505bd7ffb349a3947b
Orion Lawlor <lawlor@alaska.edu>
2025-11-02 20:30:38 -0900

    Least-squares reduce the per-chip shifts into a global camera translation and rotation, with outlier removal.

--- optical_track_openCV/main.cpp
+++ optical_track_openCV/main.cpp
@@ -1,11 +1,87 @@
 /* 
-Simple OpenCV example: does optical tracking of camera motion.
+OpenCV example: does optical tracking of camera motion.
 
 Dr. Orion Lawlor, lawlor@alaska.edu, 2025-11-01 (Public Domain)
 */
 #include <stdlib.h> /* for NULL */
 #include <iostream>  
 #include <opencv2/opencv.hpp>
+
+
+// Represents an observed camera 2D translation and rotation
+class camera_trot {
+public:
+    double dx; // shift in X (pixels)
+    double dy; // shift in Y (pixels)
+    double da; // shift in angle (radians CCW)
+};
+    
+const cv::Point2d rcen = {320, 240}; // pixel center of rotation
+const double rotscale = 1.0; // scale to make rotation in radians
+
+// Return the tangent vector when a chip at this center point gets rotated
+cv::Point2d tangent_rot(const cv::Point2d &center) {
+    cv::Point2d rv = center-rcen; // radius vector
+    cv::Point2d tv = {-rv.y,rv.x}; // tangent vector (CCW rotation)
+    return tv;
+}
+
+// Return a prediction of the pixel offset at this point
+cv::Point2d predict(const camera_trot &trot, const cv::Point2d &center)
+{
+    cv::Point2d tv = tangent_rot(center);
+    cv::Point2d pred = { 
+        trot.dx + trot.da*tv.x,
+        trot.dy + trot.da*tv.y
+    };
+    return pred;
+}
+
+// Return the pixel distance between our fit and this center point and pixel shift
+double fit_check(const camera_trot &trot, const cv::Point2d &center, const cv::Point2d &shift) 
+{
+    return cv::norm(shift - predict(trot,center));
+}
+
+// Do a least-squares fit of these camera shifts to a translation/rotation.
+//  See also: https://math.stackexchange.com/questions/2136719/least-squares-fit-to-find-transform-between-points
+camera_trot fit_trot(const std::vector<cv::Point2d> &centers,
+    const std::vector<cv::Point2d> &shifts)
+{
+    int n = (int)centers.size(); // number of points
+    int w = 3; // number of output parameters to fit: dx, dy, da
+    cv::Mat A(cv::Size(w,2*n),CV_64F); // weights
+    cv::Mat B(cv::Size(1,2*n),CV_64F); // targets
+
+    for (int i=0;i<n;i++) {
+        // Compute tangent vector for rotation at this chip center
+        cv::Point2d tv = tangent_rot(centers[i]);
+        
+        // X axis matchup
+        A.at<double>(2*i+0,0) = 1.0; // *dx -> x shift (+ pixels)
+        A.at<double>(2*i+0,1) = 0.0; // *dy
+        A.at<double>(2*i+0,2) = tv.x; // *da -> x rotate (*radians = pixels)
+        
+        B.at<double>(2*i+0,0) = shifts[i].x; // observed pixels
+
+        // Same for Y        
+        A.at<double>(2*i+1,0) = 0.0; // *dx 
+        A.at<double>(2*i+1,1) = 1.0; // *dy -> y shift
+        A.at<double>(2*i+1,2) = tv.y; // *da -> y rotate
+        
+        B.at<double>(2*i+1,0) = shifts[i].y;
+    }
+    
+    cv::Mat out;
+    cv::solve(A,B,out,cv::DECOMP_SVD);
+    
+    //std::cout<<"A "<<A<<" * out "<<out<<" = B "<<B<<"\n";
+    //std::cout<<out<<"\n";
+    camera_trot trot={out.at<double>(0,0), out.at<double>(0,1), out.at<double>(0,2)};
+    return trot;
+}
+
+
 
 
 int main(int argc,char *argv[])
@@ -46,8 +122,8 @@ int main(int argc,char *argv[])
     int nx = (vw-refWH)/stepX + 1;
     int ny = (vh-refWH)/stepY + 1;
     int nm = nx*ny;
-    std::vector<cv::Point> corr_shift(nm);
-    std::vector<cv::Point> corr_center(nm);
+    std::vector<cv::Point2d> corr_shift(nm);
+    std::vector<cv::Point2d> corr_center(nm);
     
     // An infinite loop
     while(true)
@@ -61,6 +137,9 @@ int main(int argc,char *argv[])
         if (!lastframe.empty())
         {
             if (show_debug>0) frame.copyTo(showdata);
+            
+            corr_shift.clear();
+            corr_center.clear();
         
             // Step across image, finding correlations
             for (int ix=0;ix<nx;ix++)
@@ -86,11 +165,14 @@ int main(int argc,char *argv[])
                 cv::minMaxLoc(match,0,&maxVal,0,&maxLoc);
                 
                 // Save that correlation peak
-                cv::Point shift=maxLoc-cv::Point(shiftWH,shiftWH);
-                cv::Point center=cv::Point(dx+refWH/2,dy+refWH/2);
-                int ic=ix+iy*nx; // raster index
-                corr_shift[ic]=shift;
-                corr_center[ic]=center;
+                cv::Point2d shift=cv::Point2d(maxLoc.x-shiftWH,maxLoc.y-shiftWH);
+                cv::Point2d center=cv::Point2d(dx+refWH/2,dy+refWH/2);
+                
+                if (cv::norm(shift)<0.95*shiftWH) 
+                { // plausible shift, save it
+                    corr_shift.push_back(shift);
+                    corr_center.push_back(center);
+                }
                 
                 // Save debug match image
                 if (show_debug>0) {
@@ -100,9 +182,33 @@ int main(int argc,char *argv[])
                         cv::Scalar(0,0,255),2);
                 }
             }
+            camera_trot trot={0.0,0.0,0.0};
             
-            // FIXME: outlier removal fit to corr_shift and corr_center
-            
+            printf("     Erasing: ");
+            // Outlier removal loop, to fit to corr_shift and corr_center
+            while (corr_shift.size()>12) {
+                trot=fit_trot(corr_center,corr_shift);
+                
+                // remove the worst point and re-fit
+                int worst_i=-1; double worst_err=2.0;
+                for (int i=0;i<corr_center.size();i++) {
+                    double err = fit_check(trot, corr_center[i], corr_shift[i]);
+                    //printf(" err[%d]=%.1f ",i,err);
+                    if (err>worst_err) {
+                        worst_err=err;
+                        worst_i=0;
+                    }
+                }
+                printf("\nFit: dx %8.1f   dy %8.1f    da %8.3f    n %4d\n",
+                        trot.dx, trot.dy, trot.da, (int)corr_center.size());
+                if (worst_i<0) break;
+                printf("  outlier[%d]=%.1f ",worst_i,worst_err);
+                corr_center.erase(corr_center.begin()+worst_i);
+                corr_shift.erase(corr_shift.begin()+worst_i);
+            }
+            printf("\nFinal Fit: dx %8.1f   dy %8.1f    da %8.3f    n %4d\n",
+                    trot.dx, trot.dy, trot.da, (int)corr_center.size());
+                
             if (show_debug>0) cv::imshow("Correlations",showdata);
         }
         
